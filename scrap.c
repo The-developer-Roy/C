a4365bd695683e21354f4c11908fe33c5232ffbb
the-developer-roy <roysubhadeep747@gmail.com>
2024-11-16 00:32:47 +0530

    scrap updated

--- scrap.c
+++ scrap.c
@@ -2,14 +2,23 @@
 
 int main()
 {
-    int n, i;
-    printf("Enter any number: ");
-    scanf("%d", &n);
-    while (n!=0)
-    {
-        i=n%10;
-        n/=10;
-        printf("%d\n", i);
-    }
+    int a, b, c, d, e, avg, sum;
+
+    printf("Enter your marks of five subjects one by one\n");
+    printf("Enter the marks of first subject: ");
+    scanf("%d", &a);
+    printf("Enter the marks of 2nd subject: ");
+    scanf("%d", &b);
+    printf("Enter the marks of 3rd subject: ");
+    scanf("%d", &c);
+    printf("Enter the marks of 4th subject: ");
+    scanf("%d", &d);
+    printf("Enter the marks of 5th subject: ");
+    scanf("%d", &e);
+
+    sum = a+b+c+d+e;
+    avg = sum/2;
+
+    printf("The average of your marks is: %d", avg);
     return 0;
 }
