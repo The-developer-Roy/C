981fe4c9163e366a6658159b9a68d34bf4b7c888
the-developer-roy <roysubhadeep747@gmail.com>
2024-12-01 02:00:28 +0530

    scrap modified

--- scrap.c
+++ scrap.c
@@ -1,24 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int a, b, c, d, e, avg, sum;
+    char ch;
 
-    printf("Enter your marks of five subjects one by one\n");
-    printf("Enter the marks of first subject: ");
-    scanf("%d", &a);
-    printf("Enter the marks of 2nd subject: ");
-    scanf("%d", &b);
-    printf("Enter the marks of 3rd subject: ");
-    scanf("%d", &c);
-    printf("Enter the marks of 4th subject: ");
-    scanf("%d", &d);
-    printf("Enter the marks of 5th subject: ");
-    scanf("%d", &e);
+    printf("Lowercase characters (a-z):\n");
 
-    sum = a+b+c+d+e;
-    avg = sum/2;
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        printf("%c\t", ch);
+    }
 
-    printf("The average of your marks is: %d", avg);
+    printf("\n\n");
+    
+    printf("Uppercase characters (A-Z):\n");
+
+    for (ch = 'A'; ch <= 'Z'; ch++)
+    {
+        printf("%c\t", ch);
+    }
+    
+
     return 0;
 }
